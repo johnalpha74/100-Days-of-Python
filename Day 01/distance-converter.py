5b9b803c3918b3c7244fb82887f108a4dac19d11
johnalpha <johnalpha@gmail.com>
2022-10-18 21:30:42 +0200

    CHORE: More on variable

---
+++ Day 01/distance-converter.py
@@ -0,0 +1,4 @@
+miles = input('Enter Miles ')
+miles = int(miles)
+kilometers = miles * 1.609
+print("{} miles equals {} kilometers".format(miles, kilometers))
---
+++ Day 01/exercise1.py
@@ -0,0 +1,7 @@
+#Ask user to input their name
+
+name = input('What is your name')
+
+#Print hello and name
+
+print('Hello ', name)
