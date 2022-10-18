89236a3b82b074452f7a90b8f8b4f4412dcf4e47
johnalpha <johnalpha@gmail.com>
2022-10-18 21:20:09 +0200

    CHORE: Working with Variables

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,1 +1,8 @@
 #this is my first day of 100 Days of Python
+# DAY 1: Working with Variables in Python to Manage Data
+
+print("Hello World")
+petType = input("Do you have a dog or a cat?")
+pet = input("What is the name of your Dog or cat?")
+city = input("What is the name of your city?")
+print("The name of your " + petType + " " + "is " + pet + " in " + city)
