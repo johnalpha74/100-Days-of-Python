450b57d0bd1f76cbb7ad4f26c6b887b0bdc5ce45
johnalpha <johnalpha@gmail.com>
2022-10-19 23:00:21 +0200

    Add Day 02

---
+++ Day 02/add_digits.py
@@ -0,0 +1,6 @@
+'''Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8'''
+
+two_digit_number = input("Enter two digit number:")
+
+sum = int(two_digit_number[0]) + int(two_digit_number[1])
+print(f"Sum of a two digit number  {two_digit_number} is {sum} ")
