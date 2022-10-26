6973c0ae1bc2dd204678449a682be7acfab3f827
johnalpha <johnalpha@gmail.com>
2022-10-26 20:53:39 +0200

    FEATURE: Add Day3

--- Day 03/random_name.py
+++ Day 03/random_name.py
@@ -12,7 +12,9 @@ names = namesAsCSV.split(", ")
 
 num_items = len(names)
 # Generate random numbers between 0 and the last index.
+
 random_choice = random.randint(0, num_items - 1)
+
 # Pick out random person from list of names using the random number.
 person_who_will_pay = names[random_choice]
 
